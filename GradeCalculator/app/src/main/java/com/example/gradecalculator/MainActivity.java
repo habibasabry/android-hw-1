0bf5b552e3f7d7ec758a8f3fa3dce223b98a5742
habibasabry <totado2006@gmail.com>
2022-03-07 21:16:48 +0300

    done
    I am taking care about the bonus :)

---
+++ GradeCalculator/app/src/main/java/com/example/gradecalculator/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.gradecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import javax.xml.transform.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText num1 = findViewById(R.id.Quizes);
+        EditText num2 = findViewById(R.id.Final);
+        EditText num3 = findViewById(R.id.Homeworks);
+        EditText num4 = findViewById(R.id.Midterms);
+
+        Button cal = findViewById(R.id.Calculate);
+        Button reset = findViewById(R.id.Raset);
+
+        TextView result = findViewById(R.id.Result);
+
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int Number1 = Integer.parseInt(num1.getText().toString());
+                int Number2 = Integer.parseInt(num2.getText().toString());
+                int Number3 = Integer.parseInt(num3.getText().toString());
+                int Number4 = Integer.parseInt(num4.getText().toString());
+                int total = Number1 + Number2 + Number3 + Number4;
+
+                result.setText(total + "");
+
+
+                TextView reset = findViewById(R.id.Raset);
+                reset.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        num1.setText("");
+                        num2.setText("");
+                        num3.setText("");
+                        num4.setText("");
+                        result.setText("Result");
+
+
+                    }
+                });
+
+            }
+        });
+
+
+    }
+}
